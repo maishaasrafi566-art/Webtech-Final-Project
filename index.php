225922794a675f6c712ab868711627b725403462
maishaasrafi566-art <maishaasrafi566@gmail.com>
2026-01-09 17:08:09 +0600

    index page css add'

--- index.php
+++ index.php
@@ -3,27 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <title>ABC Solutions Limited | HRM System</title>
+    <link rel="stylesheet" href="assets/css/index.css">
 </head>
 <body>
 
-    <h1>Welcome!</h1>
-    <h1>ABC Solutions Limited</h1>
-    <h2>ABC Company HRM System</h2>
+<div class="hero">
+    <div class="hero-overlay"></div>
 
-    <p>
-        Manage Employee Attendance, Leave Requests, and HR Operations
-        in a Simple and Efficient Way.
-    </p>
+    <div class="hero-content">
+        <h1 class="company-name">Welcome!</h1>
+	 <h1 class="company-name">ABC Solutions Limited</h1> 
+        <h2 class="system-title">ABC Company HRM System</h2>
 
-    <p>
-        <a href="auth/login.php">Login</a>
-        |
-        <a href="auth/register.php">Register</a>
-    </p>
+        <p class="hero-text">
+            Manage Employee Attendance, Leave Requests, and HR Operations
+            in a Simple and Efficient Way.
+        </p>
 
-    <p>
-        <a href="auth/forgot_password.php">Forgot Password?</a>
-    </p>
+        <div class="hero-buttons">
+            <a href="auth/login.php" class="btn btn-primary">Login</a>
+            <a href="auth/register.php" class="btn btn-secondary">Register</a>
+        </div>
+
+        <div class="forgot-link">
+            <a href="auth/forgot_password.php">Forgot Password?</a>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
