be7c600f83f71fd1b9df2926e3f5d42b72c5510c
maishaasrafi566-art <maishaasrafi566@gmail.com>
2026-01-09 16:40:50 +0600

    create simple index file with html

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ABC Solutions Limited | HRM System</title>
+</head>
+<body>
+
+    <h1>Welcome!</h1>
+    <h1>ABC Solutions Limited</h1>
+    <h2>ABC Company HRM System</h2>
+
+    <p>
+        Manage Employee Attendance, Leave Requests, and HR Operations
+        in a Simple and Efficient Way.
+    </p>
+
+    <p>
+        <a href="auth/login.php">Login</a>
+        |
+        <a href="auth/register.php">Register</a>
+    </p>
+
+    <p>
+        <a href="auth/forgot_password.php">Forgot Password?</a>
+    </p>
+
+</body>
+</html>
