0d52141ac0e4d4d0128d32012ce0cf1f40d7150e
maishaasrafi566-art <maishaasrafi566@gmail.com>
2026-01-09 19:28:59 +0600

    push code

---
+++ auth/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login | HRM System</title>
+</head>
+<body>
+
+    <h2>HRM Login</h2>
+    <p>Please login to continue</p>
+
+    <form method="POST">
+        <p>
+            <input type="email" name="email" placeholder="Email address" required>
+        </p>
+
+        <p>
+            <input type="password" name="password" placeholder="Password" required>
+        </p>
+
+        <p>
+            <button type="submit">Login</button>
+        </p>
+    </form>
+
+    <p>
+        <a href="forgot_password.php">Forgot Password?</a>
+    </p>
+
+    <p>
+        <a href="register.php">Create New Account</a>
+    </p>
+
+</body>
+</html>
