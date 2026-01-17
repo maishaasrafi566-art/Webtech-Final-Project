bd65d6d7610e56ca0f2b6cb5b54897112f53e604
maishaasrafi566-art <maishaasrafi566@gmail.com>
2026-01-17 15:42:06 +0600

    add html attendence_history

---
+++ Employee/attendence_history.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Attendance History</title>
+   </head>
+<body>
+
+<div class="container">
+<h2>Attendance History</h2>
+
+<table>
+<tr>
+    <th>Date</th>
+    <th>In</th>
+    <th>Out</th>
+    <th>Work Type</th>
+    <th>Status</th>
+</tr>
+
+<tr>
+    <td>YYYY-MM-DD</td>
+    <td>HH:MM:SS</td>
+    <td>HH:MM:SS / -</td>
+    <td>Work Type</td>
+    <td class="status">Status</td>
+</tr>
+
+</table>
+</div>
+
+</body>
+</html>
