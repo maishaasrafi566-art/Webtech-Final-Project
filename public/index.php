72944c3ac9319778c0559d68411ba6f674223e43
maishaasrafi566-art <maishaasrafi566@gmail.com>
2026-01-18 20:37:59 +0600

    Change Project Normal to Mvc and Change Code and Add New Public Folder with modify index as route

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../app/controllers/AuthController.php';
+require_once __DIR__ . '/../app/models/User.php';
+
+
+$url = $_GET['url'] ?? 'login';
+
+switch ($url) {
+
+    case 'login':
+        (new AuthController())->login();
+        break;
+
+    default:
+        echo "404 Page Not Found";
+}
+
